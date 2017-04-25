6c5634d00300982125ee00d445e9bc785b0e4217
Roy Sommer <roy@sommer.co.il>
2017-04-25 19:57:13 +0300

    1. Added api client 2. Added repeating-class util which handles a repeating task (will be the polling as for now)

---
+++ src/api/index.js
@@ -0,0 +1,95 @@
+import axios from 'axios';
+
+
+/**
+ * The ApiClient serves as a gateway class to the server api.
+ */
+class ApiClient {
+
+    /**
+     * @param url {string} base url of the server
+     */
+    constructor(url='http://localhost:3000') {
+
+        this._baseUrl = url;
+    }
+
+    /**
+     * Make a request to the server.
+     * @param method {string} request method
+     * @param url {string} endpoint url (without base)
+     * @param body {object} payload to be sent on body (optional)
+     * @param headers {object} headers to be passed on request (optional)
+     * @param params {object} querystring params to be sent (optional)
+     * @param credentials {{username: string, password: string}} credentials to be used
+     * @private
+     */
+    async _request(method, url, {body, headers, params, credentials}) {
+
+        // if credentials were passed, create the corresponding headers
+        if (credentials) {
+
+            headers = headers || {};
+            headers['x-user-name'] = credentials.username;
+            headers['x-user-token'] = credentials.password;
+        }
+
+        // build request description
+        let request = {};
+        request['baseUrl'] = this._baseUrl;
+        request['url'] = url;
+        request['method'] = method.toLowerCase();
+        request['headers'] = headers;
+        request['params'] = params;
+
+        // if not get, add data to payload
+        if (method.toLowerCase() != 'get') {
+
+            request['data'] = body;
+        }
+
+        // send request
+        let result;
+        try {
+
+            result = await axios(request);
+        } catch (err) {
+
+            throw new ApiException(err.message || err);
+        }
+
+        // check response
+        if (!(200 <= result.status && result.status < 300)) {
+
+            throw new ApiException(
+                result.data.message || result.data || '',
+                result.status
+            );
+        }
+
+        // return
+        return result.data;
+    }
+
+}
+
+
+/**
+ * The ApiException class represents an interface to describe errors which happened during the request flow.
+ */
+class ApiException extends Error {
+
+    /**
+     * @param message {string} message describing the error
+     * @param statusCode {number} describes the error code: the HTTP status or (-1) if the error occurred locally
+     */
+    constructor(message, statusCode=-1) {
+
+        super(message);
+        this.statusCode = statusCode
+    }
+
+}
+
+
+export {ApiClient};
---
+++ src/utils/repeating-task/index.js
@@ -0,0 +1,159 @@
+/**
+ * The RepeatingTask class takes care of carrying on tasks over repeating intervals.
+ * It ensures that no more than a single instance of the task is being run at any given moment.
+ * The tasks themselves are function handlers that will be invoked with no parameters at all.
+ */
+class RepeatingTask {
+
+    /**
+     * @param task {function()} the task to perform, can be async
+     * @param robust {boolean} if true, will continue scheduling tasks even if an iteration fails with an exception
+     */
+    constructor(task, robust=true) {
+
+        this._task = task;
+        this._robust = robust;
+
+        // interval between tasks
+        this._interval = 0;
+
+        // timeout object
+        this._nextTaskTimeout = null;
+
+        // whether or not the task is currently being run
+        this._executionLock = false;
+
+        // whether or not the routine has started
+        this._started = false;
+    }
+
+    /**
+     * Start performing the given task.
+     * @param interval {number} interval between executions in ms
+     * @param immediately {boolean} whether or not to execute the task right now
+     * @return {boolean} whether or not the task was started. false will be returned if the task is already running.
+     */
+    start(interval, immediately=true) {
+
+        if (!this._started) {
+
+            this._started = true;
+            this._interval = interval;
+
+            if (immediately) {
+
+                this.excecuteNow();
+            } else {
+
+                this._nextTaskTimeout = setTimeout(this._taskExecutionRoutine.bind(this), this._interval);
+            }
+
+            return true;
+        } else {
+
+            return false;
+        }
+    }
+
+    /**
+     * Stop executing the task.
+     */
+    stop() {
+
+        if (!this._started) {
+
+            throw new Error('RepeatingTask routine has not started, cannot stop!');
+        }
+
+        // clear the timeout
+        clearTimeout(this._nextTaskTimeout);
+
+        // denote as not started
+        this._started = false;
+    }
+
+    /**
+     * Execute an iteration of the task right now.
+     * This will do nothing if it is currently running, or cancel the scheduled run if it's sleeping.
+     */
+    excecuteNow() {
+
+        if (!this._started) {
+
+            throw new Error('RepeatingTask routine has not started, cannot execute!');
+        }
+
+        this._taskExecutionRoutine();
+    }
+
+    /**
+     * A single task execution routine.
+     * @private
+     */
+    async _taskExecutionRoutine() {
+
+        // make sure that the execution wasn't stopped prior to this
+        if (!this._started) {
+
+            return;
+        }
+
+        // attempt to acquire execution lock
+        if (!this._acquireLock()) {
+
+            return;
+        }
+
+        // clear the timeout anyways. it doesn't throw so even if there isn't a valid one, it's ok
+        clearTimeout(this._nextTaskTimeout);
+
+        // execute the task and catch if it had thrown
+        try {
+
+            await this._task();
+        } catch (err) {
+
+            // if the class was not instantiated as robust, let the exception propagate.
+            if (!this._robust) {
+
+                throw err;
+            }
+        }
+
+        // release the lock
+        this._releaseLock();
+
+        // schedule next execution only if wasn't stopped
+        if (this._started) {
+
+            setTimeout(this._taskExecutionRoutine.bind(this), this._interval);
+        }
+    }
+
+    /**
+     * Acquire the execution lock.
+     * @returns {boolean} whether the lock was acquired or not
+     * @private
+     */
+    _acquireLock() {
+
+        if (!this._executionLock) {
+
+            this._executionLock = true;
+            return true;
+        } else {
+
+            return false;
+        }
+    }
+
+    /**
+     * Releases the execution lock.
+     * @private
+     */
+    _releaseLock() {
+
+        this._executionLock = false;
+    }
+
+}
