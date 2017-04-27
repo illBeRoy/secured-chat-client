6132b3d3051dba95298228ca1fffa985e4417075
Roy Sommer <roy@sommer.co.il>
2017-04-28 00:19:01 +0300

    cryptography lib is finished

--- src/utils/cryptography/index.js
+++ src/utils/cryptography/index.js
@@ -1,5 +1,6 @@
 import forge from 'node-forge';
 import base64 from 'base-64';
+import secureRandom from 'secure-random';
 
 
 /**
@@ -7,6 +8,11 @@ import base64 from 'base-64';
  * and asymmetric encryption through RSA.
  */
 class Cryptography {
+
+    random(length=16) {
+
+        return secureRandom.randomBuffer(16);
+    }
 
     /**
      * Hashes a string using SHA-256 and returns a base-64 representation of the result.
@@ -41,26 +47,75 @@ class Cryptography {
         });
     }
 
-    encryptAsym(encryptionKey, signingKey, str) {
+    /**
+     * Encrypts a string asymmetrically.
+     *
+     * For the encryption to take place, the sender's private key and the receiver's public key are needed.
+     *
+     * First, the message is hashed and signed using the sender's private key.
+     * Then, an AES key is generated in a cryptographically random fashion, and is used to encrypt the message.
+     * After that is done, the key is encrypted using the receiver's public key.
+     * Finally, the three are concatenated and encoded in base64.
+     *
+     * @param encryptionKey {object} the receiver's public key
+     * @param signingKey {object} the sender's private key
+     * @param message {string} the message to encrypt
+     */
+    encryptAsym(encryptionKey, signingKey, message) {
 
-        // create mac using sha1
-        let sha1 = forge.sha1.create();
-        sha1.update(str);
+        // create mac with sha1 and sign it with signing key
+        let signature = signingKey.sign(forge.sha1.create().update(message));
 
-        // sign it
-        let signature = signingKey.sign(sha1);
+        // create aes cipher
+        let cipher = this.random(16);
 
-        // concatenate to message
-        let message = str + signature;
+        // encrypt message
+        let encryptedMessage = this.encryptSym(cipher, message);
 
-        // encrypt
-        let encryptedAndSignedData = encryptionKey.encrypt(forge.util.createBuffer(message), 'RSA-OAEP');
+        // encrypt cipher
+        let encryptedCipher = encryptionKey.encrypt(cipher.toString('binary'), 'RSA-OAEP');
+
+        // create whole message
+        let composedMessage = signature + encryptedCipher + encryptedMessage;
 
         // return base64 representation
-        return base64.encode(encryptedAndSignedData.bytes());
+        return base64.encode(composedMessage);
     }
 
-    decryptAsym(decryptionKey, verificationKey, str) {}
+    /**
+     * Decrypts a string asymmetrically.
+     *
+     * For the decryption to take place, the sender's public key and the receiver's private key are needed.
+     *
+     * First, the message is decoded from base64.
+     * Then, the receiver's public key is used to decrypt the cipher, which is in the first 512 bytes of the payload.
+     * This is followed by the decryption of the full message using the cipher.
+     * Finally, the decrypted message is hashed and checked against the signature using the sender's public key.
+     *
+     * @param decryptionKey {object} the receiver's private key
+     * @param verificationKey {object} the sender's public key
+     * @param encryptedMessage {string} the payload to decrypt, which is a base64 hashed message
+     */
+    decryptAsym(decryptionKey, verificationKey, encryptedMessage) {
+
+        // decode base64
+        let decodedStr = base64.decode(encryptedMessage);
+
+        // fetch and decrypt cipher
+        let cipher = decryptionKey.decrypt(decodedStr.substr(512, 512), 'RSA-OAEP');
+
+        // decrpyt message
+        let message = c.decryptSym(cipher, decodedStr.substr(1024));
+
+        // confirm signature and mac
+        if (!verificationKey.verify(forge.sha1.create().update(message).digest().bytes(), decodedStr.substr(0, 512))) {
+
+            throw new Error('Failed to verify signature');
+        }
+
+        // return
+        return message;
+    }
 
     /**
      * Encrypts a string symmetrically with a given key using AES.
@@ -106,9 +161,42 @@ class Cryptography {
         return aes.output.data;
     }
 
-    exportPublicKey(publicKey) {}
+    /**
+     * Exports a pair of keys in pem format.
+     * @param publicKey {object} public key to export (optional)
+     * @param privateKey {object} private key to export (optional)
+     * @returns {{publicKey: {object}, privateKey: {object}}}
+     */
+    exportKeys({publicKey, privateKey}) {
 
-    exportPrivateKey(privateKey) {}
+        let publicKeyPem, privateKeyPem;
+        try { publicKeyPem = forge.pki.publicKeyToPem(publicKey) } catch (err) {}
+        try { privateKeyPem = forge.pki.privateKeyToPem(privateKey) } catch (err) {}
+
+        return {
+            publicKey: publicKeyPem,
+            privateKey: privateKeyPem
+        }
+    }
+
+    /**
+     * Imports a pair of keys from pem format.
+     * @param publicKey {object} public key to import (optional)
+     * @param privateKey {object} private key to import (optional)
+     * @returns {{publicKey: {object}, privateKey: {object}}}
+     */
+    importKeys({publicKey, privateKey}) {
+
+        let importedPublicKey, importedPrivateKey;
+
+        try { importedPublicKey = forge.pki.publicKeyFromPem(publicKey) } catch (err) {}
+        try { importedPrivateKey = forge.pki.privateKeyFromPem(privateKey) } catch (err) {}
+
+        return {
+            publicKey: importedPublicKey,
+            privateKey: importedPrivateKey
+        }
+    }
 
 }
 
