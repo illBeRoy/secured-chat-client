1346c9b7b9bdd54899d97388e1ad8d59f66c1651
Roy Sommer <roy@sommer.co.il>
2017-06-06 02:24:50 +0300

    made private key integrity check more robust

--- src/backend/resources/user/actions/keys.js
+++ src/backend/resources/user/actions/keys.js
@@ -24,26 +24,32 @@ class KeysAction extends Action {
         let privateKey = null;
         if (this.username == this.session.username) {
 
-            // extract hmac and private key encryption
-            let hmac = this.privateKey.substr(-44);
-            privateKey = this.privateKey.substr(0, this.privateKey.length - 44);
+            try {
 
-            // attempt to recreate hmac locally
-            let hmacRecreation = this.utils.cryptography.hash(
-                this.utils.cryptography.encryptSym(
-                    this.session.integrityKey,
-                    privateKey
-                )
-            );
+                // extract hmac and private key encryption
+                let hmac = this.privateKey.substr(-44);
+                privateKey = this.privateKey.substr(0, this.privateKey.length - 44);
 
-            // test recreation against hmac. if failed, throw
-            if (hmacRecreation != hmac) {
+                // attempt to recreate hmac locally
+                let hmacRecreation = this.utils.cryptography.hash(
+                    this.utils.cryptography.encryptSym(
+                        this.session.integrityKey,
+                        privateKey
+                    )
+                );
+
+                // test recreation against hmac. if failed, throw
+                if (hmacRecreation != hmac) {
+
+                    throw new Error('Bad HMAC');
+                }
+
+                // decrypt own private key with data encryption key
+                privateKey = this.utils.cryptography.decryptSym(this.session.encryptionKey, privateKey);
+            } catch (err) {
 
                 throw new Error('Fatal data integrity error! Cannot validate own private key!');
             }
-
-            // decrypt own private key with data encryption key
-            privateKey = this.utils.cryptography.decryptSym(this.session.encryptionKey, privateKey);
         }
 
         // return keys
